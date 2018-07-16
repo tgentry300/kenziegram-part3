cf5158f528a6a5d1a0e7399bad1e4fb3cb699185
Taylor Gentry <taylor.gentry300@gmail.com>
2018-07-16 12:56:22 -0400

    committed

--- app.js
+++ app.js
@@ -11,28 +11,40 @@ const port = 3000;
 const app = express();
 
 const photoObject = {
-    photoArray: []
+    photoArray: [],
+    savedPhotos: [],
 }
 
 app.set('view engine', 'pug')
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(publicPath));
-const upload = multer({ dest: uploadPath });
+const upload = multer({
+    dest: uploadPath
+});
 
 
 app.post('/public/uploads', upload.single('myFile'), function (req, res, next) {
     res.render('uploads')
+    if (!photoObject.photoArray.includes(req.file.filename)) {
+        photoObject.photoArray.push(req.file.filename)
+    }
+
 })
 
-app.get("/", (req, res) => {
-    fs.readdir(uploadPath, (err, items) => {
-        items.forEach(image => {
-            photoObject.photoArray.push(image)
-        });
+app.get('/', (req, res) => {
+    fs.readdir(uploadPath, (err, files) => {
+        files.forEach(file => {
+            photoObject.photoArray.push(file)
+        })
+
+        res.render('index', {
+            items: photoObject.photoArray
+        })
+
     })
-    res.render('index', {items: photoObject.photoArray})
-})
 
+
+})
 
 app.listen(port)
